4441dea713d57cf17f6857b894dfea0f03ecd534
Amit <amit@culture-red.com>
2023-03-16 13:33:36 +0530

    Email Subject and Content change

--- app/Mail/MatchLendorRequestEmail.php
+++ app/Mail/MatchLendorRequestEmail.php
@@ -32,7 +32,7 @@ class MatchLendorRequestEmail extends Mailable
     	$authUser = auth()->user();
        
         return $this->from(config('mail.from.address'), 'Render')
-		->subject('Deep discount referral fee 17% paid at closing home buyer leads for Realtors.')
+		->subject('Deep discount referral fee paid at closing home buyer leads for Realtors.')
         ->markdown('email.matching.lendor-match-request');
         
     }
