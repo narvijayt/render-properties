9016ee6845e4838bc1157e25f226e33cdd4254d8
Amit <amit@culture-red.com>
2023-03-29 16:03:59 +0530

    Hide rating dummy text from user profile view page

--- resources/views/pub/partials/users/user-profile-details.blade.php
+++ resources/views/pub/partials/users/user-profile-details.blade.php
@@ -20,7 +20,7 @@ $match = (!isset($match)) ? false : $match;
             <h3 class="text-warning text-uppercase mt-0">{{ title_case($user->user_type === 'broker' ? 'lender' : 'real estate agent') }}</h3>
 
             <div class="text-uppercase d-flex">
-                <div class="col">Render Rating: </div>
+                <!-- <div class="col">Render Rating: </div> -->
                 <div class="col"><h5 class="m-0">4.75 (Out of 5)</h5></div>
             </div>
         </div>
