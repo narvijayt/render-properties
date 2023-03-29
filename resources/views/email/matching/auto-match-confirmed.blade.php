086858b0897f2bca90a10d084f17590e282e8406
Amit <amit@culture-red.com>
2023-03-29 16:16:13 +0530

    Auto match Confirmed email template text

--- resources/views/email/matching/auto-match-confirmed.blade.php
+++ resources/views/email/matching/auto-match-confirmed.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-<p>Hey {{ ucfirst($brokerUser->first_name) }},<br/> Congratulations, a Real Estate Agent in your area, {{ $realtorUser->first_name.' '.$realtorUser->last_name }} has connected with you. Click on the link below to see details. </p>
+<p>Hey {{ ucfirst($brokerUser->first_name) }},<br/> Congratulations, a Real Estate Agent in your area, <strong>{{ $realtorUser->first_name.' '.$realtorUser->last_name }}</strong> has connected with you. Click on the link below to see details. </p>
 
 
 @component('mail::button', ['url' =>  route('realtordetails.automatch', ['brokerId' => $brokerUser->user_id, 'realtorId' => $realtorUser->user_id]) ])
