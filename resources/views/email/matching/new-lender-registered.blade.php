a5cc2f7c77c0e3c916611317af245e910279a57f
Amit <amit@culture-red.com>
2023-03-31 19:45:31 +0530

    Email template for Reltor and Lender in auto match case are updated

--- resources/views/email/matching/new-lender-registered.blade.php
+++ resources/views/email/matching/new-lender-registered.blade.php
@@ -1,5 +1,9 @@
 @component('mail::message')
-<p>Hey {{ ucfirst($realtorUser->first_name) }},<br/>Time to increase your sales! Congratulations, a Loan Officer in your area, <strong>{{ $brokerUser->first_name.' '.$brokerUser->last_name }}</strong> wants to connect with you. Click on the link below to see the details. </p>
+<p>Hey {{ ucfirst($realtorUser->first_name) }},</p>
+
+<p>Do you work with first time home buyers? <strong>{{ $brokerUser->first_name.' '.$brokerUser->last_name }}</strong> is seeking a realtor partner match for Render’s home buyer lead program. </p>
+
+<p>The leads have zero upfront fees for Realtors. Click on the link below to see details</p>
 
 
 @component('mail::button', ['url' =>  route('view.automatch', ['brokerId' => $brokerUser->user_id, 'realtorId' => $realtorUser->user_id]) ])
