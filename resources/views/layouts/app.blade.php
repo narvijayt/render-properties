fb06b227808ba1885c5ae4c45402d31a9899df3c
Amit <amit@culture-red.com>
2023-04-17 18:04:36 +0530

    Remove extra script tags, add new analytical code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,7 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	    <!-- Google Tag Manager -->
-	<script>
-		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','{{ config('google.tagmanager.key') }}');
-	</script>
-		<!-- End Google Tag Manager -->
+	    
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title>@yield('title', 'Default') | {{ config('app.name', 'Render') }}</title>
 	<link rel="icon" href="/img/icon.png" sizes="32x32" type="image/png">
@@ -46,98 +39,20 @@
 	@endif
 	@stack('styles-header')
 	@stack('scripts-header')
-		<!-- Place the following in the HEAD before BODY tag -->
-		<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-74K9XE4F27"></script>
 		<script>
-			var googletag = googletag || {};
-			googletag.cmd = googletag.cmd || [];
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-74K9XE4F27');
 		</script>
-		<script>
-		googletag.cmd.push(function() {
-			googletag.defineSlot('/21803544343/RBC-HP', [300, 600], 'div-gpt-ad-1552396650444-0').addService(googletag.pubads());
-			googletag.pubads().enableSingleRequest();
-			googletag.pubads().collapseEmptyDivs();
-			googletag.enableServices();
-		});
-		</script>
-		<script>
-		googletag.cmd.push(function() {
-			googletag.defineSlot('/21803544343/RBC-LL1', [970, 90], 'div-gpt-ad-1552396960512-0').addService(googletag.pubads());
-			googletag.pubads().enableSingleRequest();
-			googletag.pubads().collapseEmptyDivs();
-			googletag.enableServices();
-		});
-		</script>
-	<script>
-		googletag.cmd.push(function() {
-			googletag.defineSlot('/21803544343/RBC-LR1', [336, 280], 'div-gpt-ad-1552397044238-0').addService(googletag.pubads());
-			googletag.pubads().enableSingleRequest();
-			googletag.pubads().collapseEmptyDivs();
-			googletag.enableServices();
-		});
-		</script>
-		<script>
-		googletag.cmd.push(function() {
-			googletag.defineSlot('/21803544343/RBC-LB1', [728, 90], 'div-gpt-ad-1552397119304-0').addService(googletag.pubads());
-			googletag.pubads().enableSingleRequest();
-			googletag.pubads().collapseEmptyDivs();
-			googletag.enableServices();
-		});
-		</script>
-		<script>
-		googletag.cmd.push(function() {
-			googletag.defineSlot('/21803544343/RBC-MR1', [300, 250], 'div-gpt-ad-1552397962554-0').addService(googletag.pubads());
-			googletag.pubads().enableSingleRequest();
-			googletag.pubads().collapseEmptyDivs();
-			googletag.enableServices();
-		});
-		</script>
-		<script>
-			googletag.cmd.push(function() {
-				googletag.defineSlot('/21803544343/RBC-MB1', [320, 50], 'div-gpt-ad-1552397818666-0').addService(googletag.pubads());
-				googletag.pubads().enableSingleRequest();
-				googletag.pubads().collapseEmptyDivs();
-				googletag.enableServices();
-			});
-		</script>
-	@if(url()->current() == 'https://www.render.properties')
-		<!-- Conversion Pixel for [misc]- DO NOT MODIFY -->
-		<img style="display:none;" src="https://data.adxcel-ec2.com/pixel/?ad_log=referer&action=misc&pixid=6ebfe037-db9d-4717-bb0b-4bb3c4d8e135" width="1" height="1" border="0">
-		<!-- End of Conversion Pixel -->
-	@elseif(Request::segment(1) === 'contact')
-		<!-- Conversion Pixel for [content]- DO NOT MODIFY -->
-		<img style="display:none;" src="https://data.adxcel-ec2.com/pixel/?ad_log=referer&action=content&pixid=6ebfe037-db9d-4717-bb0b-4bb3c4d8e135" width="1" height="1" border="0">
-		<!-- End of Conversion Pixel -->
-	@elseif(Request::segment(1) === 'search-profiles')
-		<!-- Conversion Pixel for [registration]- DO NOT MODIFY -->
-		<img style="display:none;" src="https://data.adxcel-ec2.com/pixel/?ad_log=referer&action=registration&pixid=6ebfe037-db9d-4717-bb0b-4bb3c4d8e135" width="1" height="1" border="0">
-		<!-- End of Conversion Pixel -->
-	@elseif(isset($_GET['type']) && $_GET['type'] === 'realtor')   
-		<!-- Conversion Pixel for [signup]- DO NOT MODIFY -->
-		<img style="display:none;" src="https://data.adxcel-ec2.com/pixel/?ad_log=referer&action=signup&pixid=6ebfe037-db9d-4717-bb0b-4bb3c4d8e135" width="1" height="1" border="0">
-		<!-- End of Conversion Pixel -->
-	@elseif(isset($_GET['type']) && $_GET['type'] === 'lender')  
-		<!-- Conversion Pixel for [lead]- DO NOT MODIFY -->
-		<img style="display:none;" src="https://data.adxcel-ec2.com/pixel/?ad_log=referer&action=lead&pixid=6ebfe037-db9d-4717-bb0b-4bb3c4d8e135" width="1" height="1" border="0">
-		<!-- End of Conversion Pixel -->
-	@endif
 		<link href="{{ asset('css/ionicons.min.css') }}" rel="stylesheet">  
 	</head>
 	
 	<body>
-		<!-- Google Tag Manager (noscript) -->
-		<noscript>
-		<iframe src="https://www.googletagmanager.com/ns.html?id={{ config('google.tagmanager.key') }}" height="0" width="0" style="display:none;visibility:hidden">
-		</iframe>
-	</noscript>
-	<!-- End Google Tag Manager (noscript) -->
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<script>
-		(adsbygoogle = window.adsbygoogle || []).push({
-			google_ad_client: "ca-pub-6901510063192256",
-			enable_page_level_ads: true
-		});
-	</script>
+		
 	@if(url()->current() == 'https://www.render.properties' || url()->current() == 'https://www.render.properties' || url()->current() == 'www.render.properties')
 		<div id="app">
 	@elseif(Request::segment(1) === 'about')
@@ -229,26 +144,5 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-lightbox/0.7.0/bootstrap-lightbox.min.js"></script>
 	@stack('scripts-footer')
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('google.analytics.key') }}"></script>
-		<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-			gtag('config', '{{ config('google.analytics.key') }}');
-		</script>
-		<script type="text/javascript">
-			window.__lc = window.__lc || {};
-			window.__lc.license = 11648588;
-			(function() {
-				var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-				lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-			})();
-		</script>
-		<noscript>
-			<a href="https://www.livechatinc.com/chat-with/11648588/" rel="nofollow">Chat with us</a>,
-			powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
-		</noscript>
 	</body>
 </html>
