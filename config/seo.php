04344212c06b58d3df6bd3080625a9a078ba683a
Amit <amit@culture-red.com>
2024-01-17 18:02:42 +0530

    SEO image updated

--- config/seo.php
+++ config/seo.php
@@ -2,7 +2,7 @@
 
 return [
 	'description' => 'Join a growing community of Realtors and Lenders who are looking to change the way they do business ',
-	'image' => env('APP_URL') . '/img/render-seo-logo-2.png',
+	'image' => env('APP_URL') . '/img/render-properties.jpeg',
 	'facebook_id' => '',
 	'twitter_handle' => '@realbrokerconnection',
 	'keyword' => "Render,Real Estate Agent,Vendor,Home Buyers,Mortgage Loan Officers ,Home Service Providers,Sell Homes,Broker,Commercial,Invest,Investing,Investor",
