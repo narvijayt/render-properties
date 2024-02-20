a3e3a64023da4364aee36ec9259b4e85596db29a
Amit <amit@culture-red.com>
2024-02-20 12:28:36 +0530

    Update Coupon only if sale price added

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -104,7 +104,11 @@ class SettingsController extends Controller
                 $pricing = new RegistrationPlans();
             }else{
                 $planId = ($pricing->planId != "") ? $pricing->planId : $planId;
-                // $couponId = $createCoupon == true ? $coupon->id : $pricing->couponId;
+                if($request->input('sale_price') == ""){
+                    $couponId = "";
+                }else{
+                    $couponId = isset($coupon) ? $coupon->id : $pricing->couponId;
+                }
             }
 
             $pricing->regular_price = $request->input('regular_price');
