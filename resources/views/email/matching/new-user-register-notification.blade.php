07f5a699640c646f00d6fea714fada31258f8ff8
Amit <amit@culture-red.com>
2024-03-21 15:36:30 +0530

    Email content and sms content change

--- resources/views/email/matching/new-user-register-notification.blade.php
+++ resources/views/email/matching/new-user-register-notification.blade.php
@@ -3,7 +3,7 @@
 
 <p>We're excited to inform you that a new {{ $usertype }} has recently registered in your area! This presents a great opportunity for you to expand your network and connect with more home buyers and sellers who need your services. </p>
 
-<p>To match with this new member and start connecting, simply click on the link below:</p>
+<p>To match with this new member and start connecting, simply click on the <strong>View Details</strong> button below:</p>
 
 @component('mail::button', ['url' =>  route('pub.user.show', ['user_id' => $user->user_id]) ])
 View Details
@@ -12,8 +12,5 @@ View Details
 <p>Don't miss out on this chance to grow your business and reach more clients. We look forward to seeing the successful connections you make!</p>
 
 Best regards,<br>
-{{ $member->first_name.' '.$member->last_name }},<br>
-{{ $memberType }},<br>
-@if(!empty($member->phone_number))
-    Phone: {{ $member->phone_number }},<br>
-@endif
+Team {!! get_application_name() !!}
+@endcomponent
